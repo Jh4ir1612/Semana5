72f316f768024c1aa0c3c20e6d349f7fa43ace6a
Jhair Vasquez Huaman <e_2020101659B@uncp.edu.pe>
2025-05-27 19:39:17 -0500

    semana5

--- laboratorio05/src/components/Card.js
+++ laboratorio05/src/components/Card.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import 'components/Card.css'; // Assuming you have a CSS file for styles
-
 const Card = ({ title, description, imageUrl, children }) => {
-  return (
+return (
     <div className="card">
-      {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
-      <div className="card-content">
+    {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
+    <div className="card-content">
         <h3>{title}</h3>
         <p>{description}</p>
         {children && <div className="card-actions">{children}</div>}
-      </div>
     </div>
-  );
+    </div>
+);
 };
 
 export default Card;
--- laboratorio05/src/components/Container.js
+++ laboratorio05/src/components/Container.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import 'components/Container.css'; // Assuming you have a CSS file for styles
-
 const Container = ({ children, borderColor = '#ccc' }) => {
-  return (
+return (
     <div className="container" style={{ borderColor }}>
-      {children}
+    {children}
     </div>
-  );
+);
 };
 
 export default Container;
